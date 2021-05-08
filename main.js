2bb00f6c3eeed5269b4deb2e9a7f2b06dcefa0d4
Sara <sara_mohamoud@outlook.com>
2021-05-08 15:41:43 +0100

    re-started

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
-async function getPokemon(string) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${string}/`);
+
+// let userInput = document.getElementById("user-input").value;
+// let pokemonToSearch= null;
+
+
+
+// function handleUserinput(){
+// pokemonToSearch = document.querySelector("#user-choice").value
+
+
+
+async function getPokemon() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu/");
     const data = await response.json();
     console.log(data);
     let pokemonName = data.name;
     addPokemonToTheList(pokemonName);
 }
+getPokemon();
+// console.log(userInput)
 
 const pokemonCollection = document.getElementById("pokemon-collection");
 
 function addPokemonToTheList(string) {
     let li = document.createElement("li");
     let listItem = document.createTextNode(string);
+    let pikachuImage = document.createElement("img");
     li.appendChild(listItem);
     pokemonCollection.appendChild(li);
+
 }
 
-let userInput = document.getElementById("user-input").nodeValue;
 
-const addPokemon = document.getElementById("add-pokemon");
 
-addPokemon.addEventListener("click", handleClick);
+// const addPokemon = document.getElementById("add-pokemon");
+
+// addPokemon.addEventListener("click", getPokemon);
     
-function handleClick() {
-    getPokemon(userInput);
-}
+// function handleClick(userInput) {
+//     getPokemon(userInput);
+// }
